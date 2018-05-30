d4893b67c8a688df218e5e78df1ee7e274b2425c
Wataru NAKAI <watnk0806@gmail.com>
2018-05-30 21:11:40 +0900

    Edit setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 setup(
+    name='nbconvert_export_r',
+    version='0.0.1',
+    description='Custom exporter of nbconvert for R language',
+    author='whatalnk',
+    url='https://github.com_whatalnk/nbconvert_export_r',
+    packages=find_packages(),
     entry_points={
         'nbconvert.exporters': [
             'r = nbconvert_exporter_r:RExporter'
