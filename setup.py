da1460f333043c285670a0e09dfbcafe3ec2156a
Wataru NAKAI <watnk0806@gmail.com>
2018-05-30 23:24:17 +0900

    Change entry_points

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nbconvert_exporter_r',
-    version='0.0.2',
+    version='0.0.3',
     description='Custom exporter of nbconvert for R language',
     author='whatalnk',
     url='https://github.com_whatalnk/nbconvert_exporter_r',
@@ -10,9 +10,12 @@ setup(
     package_data={
         '': ['templates/*.tpl'],
     },
+    # Because there is no 'language_info' in metadata of IRKernel
+    # not 'nbconvert.exporters' but 'nbconvert.exporters.script'
+    # and the key is case sensitive
     entry_points={
-        'nbconvert.exporters': [
-            'r = nbconvert_exporter_r:RExporter'
+        'nbconvert.exporters.script': [
+            'R = nbconvert_exporter_r:RExporter'
         ],
     }
 )
